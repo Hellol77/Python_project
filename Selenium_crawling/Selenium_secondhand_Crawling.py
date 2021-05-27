c2314cfa4c33ff17aa8ab12293ccc2821be3005e
Hellol77 <dhe7700@naver.com>
2021-05-28 01:01:38 +0900

    update selenium secondHandCrawling

--- Selenium_crawling/Selenium_secondhand_Crawling.py
+++ Selenium_crawling/Selenium_secondhand_Crawling.py
@@ -6,12 +6,16 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+options = webdriver.ChromeOptions()
+# 창 숨기는 옵션 추가
+options.add_argument("headless")
+
 class Second_hand_crawler(object):
 	def __init__(self,query,page_num):
 		self.query = query#물건 이름
 		self.page_num =page_num#1페이지 부터 15페이지 까지 검색할 예정
 		self.url=f"https://cafe.naver.com/ca-fe/home/search/c-articles?q={query}&p={page_num}"
-		self.driver = webdriver.Chrome('C:/Users/Hellol/Desktop/Coding/Python_project/Selenium_crawling/chromedriver.exe')
+		self.driver = webdriver.Chrome('C:/Users/Hellol/Desktop/Coding/Python_project/Selenium_crawling/chromedriver.exe',options=options)
 		
 	def load_page(self):
 		driver = self.driver
@@ -19,16 +23,19 @@ class Second_hand_crawler(object):
 		driver.get(self.url)
 		name_data = driver.find_elements_by_class_name("subject_text")#제품 이름
 		price_data= driver.find_elements_by_class_name("item_info_area")#가격과 각종 정보들
-		for name,price in zip(name_data,price_data):
+		
+		
+		for name,price in zip(name_data,price_data):	
 			print(name.text)
 			print(price.text)
 			print('')
 			
 	def close_web(self):#열린 웹들을 자동으로 닫게한다
-		self.driver.close()
+		self.driver.quit()
 		
 staff=input("중고구매할 제품의 이름을 입력하세요 : ")
-for i in range(16):
+page=int(input("몇 페이지를 검색할까요? : "))
+for i in range(1,page+1):
 	crawler=Second_hand_crawler(staff,i)
 	crawler.load_page()
 	crawler.close_web()
